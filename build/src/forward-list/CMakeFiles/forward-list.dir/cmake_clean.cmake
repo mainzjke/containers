99a59f413ffa8fa49e759054ce25198b4ca92bb9
mainzjke <andrey.kurnikov1998@mail.ru>
2024-06-08 13:06:34 +0400

    deleted shit

--- build/src/forward-list/CMakeFiles/forward-list.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "../../../lib/libforward-list.pdb"
-  "../../../lib/libforward-list.so"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/forward-list.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
